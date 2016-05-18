ef0dbd2956204678377868d5c382e3acffb2367b
Matt Bell <mappum@gmail.com>
2016-05-17 23:28:39 -0700

    Reduce travis script

--- travis-script.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo $PATH
-source /tmp/.nvm/nvm.sh
-echo $PATH
-nvm use default
-node --version
-npm --version
-npm test
--- travis.sh
+++ travis.sh
@@ -4,13 +4,15 @@ if [ -z $NODE ]; then
   export NODE=6
 fi
 
+echo Installing nvm...
 git clone https://github.com/creationix/nvm.git /tmp/.nvm
 source /tmp/.nvm/nvm.sh
+echo Installed nvm version `nvm --version`
 nvm install $NODE
 nvm use $NODE
 nvm alias default $NODE
-nvm --version
-node --version
-npm --version
+echo node version: `node --version`
+echo npm version: `npm --version`
 npm install
-echo $PATH
+echo Install completed
+npm test
