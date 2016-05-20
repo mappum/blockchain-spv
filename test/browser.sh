ebca1c5be502021dc8ae756e19da1c2c2993b58a
Matt Bell <mappum@gmail.com>
2016-05-20 15:19:55 -0700

    Use zuul-ngrok to tunnel to saucelabs

--- test/browser.sh
+++ test/browser.sh
@@ -5,6 +5,7 @@ if [ $BROWSER ]; then
     --browser-name $BROWSER \
     --browser-version latest \
     --ui tape \
+    --tunnel ngrok \
     -- test/*.js
 else
   zuul --local --ui tape -- test/*.js
