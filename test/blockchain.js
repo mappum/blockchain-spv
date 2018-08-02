bce625f3bdd4a8210cfa156ccc1272cf6b79ce61
Matt Bell <mappum@gmail.com>
2018-08-01 18:23:41 -0700

    Added first test case so tests pass

--- test/blockchain.js
+++ test/blockchain.js
@@ -1,4 +1,14 @@
 'use strict'
 
-// const test = require('tape')
-// const Blockchain = require('../lib/blockchain.js')
+var test = require('tape')
+var Blockchain = require('../lib/blockchain.js')
+
+test('create Blockchain instance with no args', function (t) {
+  try {
+    let chain = new Blockchain()
+    t.undefined(chain)
+  } catch (err) {
+    t.equal(err.message, 'Must specify starting header')
+  }
+  t.end()
+})
