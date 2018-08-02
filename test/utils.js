c6faeeb81477c9209d0a8faed6320212b8e30f7a
Matt Bell <mappum@gmail.com>
2018-08-01 17:58:13 -0700

    Removed more dependencies

--- test/utils.js
+++ test/utils.js
@@ -1,26 +1,22 @@
-var bitcoinjs = require('bitcoinjs-lib')
-var u = require('bitcoin-util')
-var reverse = require('buffer-reverse')
-var assign = require('object-assign')
-var params = require('webcoin-bitcoin').blockchain
+'use strict'
 
-exports.blockFromObject = function (obj) {
-  return assign(new bitcoinjs.Block(), obj)
-}
+const bitcoinjs = require('bitcoinjs-lib')
+const u = require('bitcoin-util')
+const params = require('webcoin-bitcoin').blockchain
 
 exports.createBlock = function (prev, nonce, bits, validProof) {
   var i = nonce || 0
   validProof = validProof == null ? true : validProof
   var header
   do {
-    header = exports.blockFromObject({
+    header = {
       version: 1,
       prevHash: prev ? prev.getHash() : u.nullHash,
       merkleRoot: u.nullHash,
       timestamp: prev ? (prev.timestamp + 1) : Math.floor(Date.now() / 1000),
       bits: bits || (prev ? prev.bits : u.compressTarget(exports.maxTarget)),
       nonce: i++
-    })
+    }
   } while (validProof !== exports.validProofOfWork(header))
   return header
 }
@@ -29,7 +25,7 @@ exports.maxTarget = new Buffer('7fffffffffffffffffffffffffffffffffffffffffffffff
 
 exports.validProofOfWork = function (header) {
   var target = u.expandTarget(header.bits)
-  var hash = reverse(header.getHash())
+  var hash = header.getHash().reverse()
   return hash.compare(target) !== 1
 }
 
@@ -38,14 +34,14 @@ exports.createBlock = function (prev, nonce, bits, validProof) {
   validProof = validProof == null ? true : validProof
   var header
   do {
-    header = exports.blockFromObject({
+    header = {
       version: 1,
       prevHash: prev ? prev.getHash() : u.nullHash,
       merkleRoot: u.nullHash,
       timestamp: prev ? (prev.timestamp + 1) : Math.floor(Date.now() / 1000),
       bits: bits || (prev ? prev.bits : u.compressTarget(exports.maxTarget)),
       nonce: i++
-    })
+    }
   } while (validProof !== exports.validProofOfWork(header))
   return header
 }
